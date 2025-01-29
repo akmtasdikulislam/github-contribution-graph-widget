e8cd0a5516de290a29baa6d53c7548c7fafd6950
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-01-29 16:33:22 +0600

    updated GitHubUser component

--- src/components/GithubUser/GithubUser.jsx
+++ src/components/GithubUser/GithubUser.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import avatar from "../../assets/images/akmtasdikulislam.jpg";
+import avatar from "../../assets/images/deafult-user.png";
 
 /**
  * GithubUser Component
@@ -15,43 +15,69 @@ import avatar from "../../assets/images/akmtasdikulislam.jpg";
  * <GithubUser username="akmtasdikulislam" />
  */
 
+import { EyeIcon, SyncIcon } from "@primer/octicons-react";
 import PropTypes from "prop-types";
 
 const GithubUser = ({ username }) => {
   // State for user data
   const [avatarUrl, setAvatarUrl] = useState("");
   const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   /**
    * Fetches user data from GitHub API
    */
+  const fetchUserData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const data = await response.json();
+      setAvatarUrl(data.avatar_url);
+      setName(data.name);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching GitHub user data:", error);
+    }
+  };
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch(
-          `https://api.github.com/users/${username}`
-        );
-        const data = await response.json();
-        setAvatarUrl(data.avatar_url);
-        setName(data.name);
-      } catch (error) {
-        console.error("Error fetching GitHub user data:", error);
-      }
-    };
-
     fetchUserData();
-  }, [username]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div className="github-user">
+    <div className={`github-user ${isLoading && "fetchingUserData"}`}>
       <img
         className="github-user-avarter"
         src={avatarUrl || avatar}
         alt={`${name}'s avatar`}
       />
       <div className="github-user-details">
-        <p className="github-user-name">{name || "Akm Tasdikul Islam"}</p>
+        <p className="github-user-name">{name || "GitHub User Name"}</p>
         <p className="github-user-username">@{username}</p>
+      </div>
+      <div className="overlay">
+        {!isLoading ? (
+          <div className="button-group">
+            <button
+              title="View Account"
+              onClick={() =>
+                window.open(`https://github.com/${username}`, "_blank")
+              }
+            >
+              <EyeIcon />
+            </button>
+            <button
+              title="Sync"
+              onClick={() => {
+                fetchUserData();
+              }}
+            >
+              <SyncIcon />
+            </button>
+          </div>
+        ) : (
+          <div className="loading-dots">Syncing</div>
+        )}
       </div>
     </div>
   );
